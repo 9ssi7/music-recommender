f1693e448b43fe1776a04d5e16f9a3dea01a92eb
9ssi7 <info@ssibrahimbas.com>
2024-04-28 22:18:48 +0300

    feat: update the main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/9ssi7/music-recommender/config"
 	"github.com/9ssi7/music-recommender/pkg/clients/neo4j"
+	"github.com/9ssi7/music-recommender/server/graph"
+	"github.com/9ssi7/music-recommender/service"
 )
 
 func main() {
 	cnf := config.Get()
 	driver := neo4j.Connect(cnf.Neo4j)
-	fmt.Println(driver.Target().Scheme)
+	app := service.NewApp(service.Config{
+		Driver: driver,
+	})
+	graphSrv := graph.NewServer(cnf.Graphql, app)
+	graphSrv.Listen()
 }
